fb534ebd5d35c4862e0c9a2e690502aa6a2c4786
Dark-Ruzi <pasindusbandara923@gmail.com>
2025-05-13 17:04:56 +0530

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ KHAN ┃ MD ✪──";
+        teks += "└──✪ 𝐅𝚫𝚰𝐒𝚫𝐋-𝚳𝐃 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
